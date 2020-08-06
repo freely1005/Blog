ee1206cb7893bb7a4a2b9e54beea113caa512891
2285811257 <root123>
2020-08-06 22:41:22 +0800

    初始化

---
+++ view/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    title: '江南一点雨',
+    head: [
+        ['link', {rel: 'icon', href: 'https://open.weixin.qq.com/qr/code?username=a_javaboy'}]
+    ],
+    themeConfig: {
+        logo: 'https://open.weixin.qq.com/qr/code?username=a_javaboy',
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '国际站', link: 'http://www.javaboy.org'},
+            {text: '国内站', link: 'http://www.itboyhub.com'}
+        ],
+        sidebar: 'auto',
+        sidebar: [
+            {
+                title: 'Java',
+                path: '/java/',
+                collapsable: false,
+                sidebarDepth: 2,
+                children: [
+                    '/java/java-1',
+                    '/java/java-2'
+                ]
+            },
+            {
+                title: 'Python',
+                path: '/python/',
+                collapsable: false,
+                sidebarDepth: 2,
+                children: [
+                    '/python/python-1',
+                    '/python/python-2'
+                ]
+            },
+        ]
+
+    },
+
+};
